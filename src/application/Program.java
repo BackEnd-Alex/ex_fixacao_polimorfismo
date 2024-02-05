359a9f9acd50c68c8f211eb46503edc59f2b19f4
BackEnd-alex <contadetestealexsandro@gmail.com>
2024-02-04 21:20:24 -0300

    atualizando codigos

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,2 +1,46 @@
-package application;public class Program {
+package application;
+
+import entities.Employee;
+import entities.OutsourcedEmployee;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Scanner leia = new Scanner(System.in);
+        Locale.setDefault(Locale.US);
+        List<Employee>list = new ArrayList<>();
+
+        System.out.print("Enter the number of employee:");
+        int n = leia.nextInt();
+        for (int i = 1; i <= n; i++){
+            System.out.println("Employee #"+ i + " data:");
+            System.out.print("Outsourced (y/n) ?");
+            char ch = leia.next().charAt(0);
+            System.out.print("Name: ");
+            leia.nextLine();
+            String name = leia.nextLine();
+            System.out.print("Hours: ");
+            int hours = leia.nextInt();
+            System.out.print("Value per hours: ");
+            double valuePerHour = leia.nextDouble();
+            if(ch == 'y'){
+                System.out.print("Additional charge: ");
+                double addtional = leia.nextDouble();
+               // Employee emp = new OutsourcedEmployee(name, hours,valuePerHour,addtional);
+                //list.add(emp);
+                list.add(new OutsourcedEmployee(name, hours,valuePerHour,addtional));
+            }
+            else {
+                list.add(new Employee(name,hours,valuePerHour));
+            }
+        }
+        System.out.println("PAYMENTS: ");
+        for (Employee emp: list) {
+            System.out.println(emp.getName() + "- $ " + String.format("%.2f", emp.payments()));
+        }
+    }
 }
--- src/entities/OutsourcedEmployee.java
+++ src/entities/OutsourcedEmployee.java
@@ -1,2 +1,23 @@
-package entities;public class OutsourcedEmployee {
+package entities;
+
+public class OutsourcedEmployee extends Employee {
+    private Double addtionalCharge;
+
+    public OutsourcedEmployee(String name, Integer hours, Double valuePerHours, Double addtionalCharge) {
+        super(name, hours, valuePerHours);
+        this.addtionalCharge = addtionalCharge;
+    }
+
+    public Double getAddtionalCharge() {
+        return addtionalCharge;
+    }
+
+    public void setAddtionalCharge(Double addtionalCharge) {
+        this.addtionalCharge = addtionalCharge;
+    }
+    @Override
+    public Double payments(){
+        return super.payments() + addtionalCharge * 1.1;
+    }
 }
+
